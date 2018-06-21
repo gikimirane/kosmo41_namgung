397c78a357d2394c27cb649433d000dc1e09fbcb
YoonHee(S) <yh9189@gmail.com>
2018-06-21 09:50:19 +0900

    자바수업

---
+++ Java/ch01/BlockComment.java
@@ -0,0 +1,21 @@
+/*
+이름 : 남궁윤희
+작성일 : 2018.6.20
+*/
+
+class BlockComment
+{
+	public static void main(String[] args)
+	{
+		/*단순한 정수의 출력*/
+		System.out.println(7);
+		System.out.println(3.15);
+		/*덧셈 결과의 출력*/
+		System.out.println(3 + 5);
+		System.out.println("3 + 5" + "의 연산 결과는 8 입니다.");
+		System.out.println("3 + 5 = " + 8);
+		System.out.println(3.15 + "는 실수 입니다.");
+		
+		
+	}
+}
---
+++ Java/ch02/src/ch02/OperatePromotion.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class OperatePromotion {
+
+	public static void main(String[] args) {
+		Short num1 = 11;
+		Short num2 = 22;
+		short result = num1 + num2;
+		System.out.println(result);
+
+	}
+
+}
