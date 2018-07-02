bd586136d48acd3ea9d914719549e3370e53d500
Yoonhee(H) <yh9189@gmail.com>
2018-07-03 01:45:41 +0900

    과제 조금 수정

--- Java/Game/src/by3_Game.java
+++ Java/Game/src/by3_Game.java
@@ -3,71 +3,85 @@ import java.util.Scanner;
 class Move {
 	String[][] MoveU(String arr[][]) {	
 		String[][] temp = new String[3][3];
-		for(int i=0;i<3;i++) {
-			int n=1;
+		int n=1;
+		for(int i=2;i>0;i--) {
 			for(int j=0;j<3;j++) {
-				if(i==0) n=0;
-				if(arr[i+n][j].equals("x")) {
-					temp[i][j] = arr[i][j];  //backup 
-					arr[i][j] = arr[i+n][j]; //i,j에 x값을 넣음
-					arr[i+n][j] = temp[i][j];
+				if(i==0 && arr[i][j].equals("x")) {
+					System.out.println("뭐 출력하게?"+(i-n)+","+j+","+arr[i-n][j]);
+					temp[i+n][j] = arr[i+n][j]; //i,j에 x값을 넣음
+					arr[i+n][j] = arr[i][j];
+					arr[i][j] = temp[i+n][j];	
 				}
+				else if(arr[i-n][j].equals("x")) {
+					temp[i-n][j] = arr[i-n][j]; //i,j에 x값을 넣음
+					arr[i-n][j] = arr[i][j];
+					arr[i][j] = temp[i-n][j];
+				}	
 			}
-			n++;
 		}
 		return arr;
 	}
 	String[][] MoveD(String arr[][]) {
 		
 		String[][] temp = new String[3][3];
-		for(int i=2;i>=0;i--) {
-			int n=1;
-			for(int j=0;j<3;j++) {
-				if(i==0) n=0;
-				if(arr[i-n][j].equals("x")) {
-					temp[i][j] = arr[i][j];  //backup 
-					arr[i][j] = arr[i-n][j]; //i,j에 x값을 넣음
-					arr[i-n][j] = temp[i][j];
-				}
-			}
-			n++;
-		}
-		return arr;
-	}
-	String[][] MoveR(String arr[][]) {
-		String[][] temp = new String[3][3];
+		int n=1;
 		for(int i=0;i<3;i++) {
-			int n=1;
 			for(int j=0;j<3;j++) {
-				if(j==2) n=0;
-				if(arr[i][j+n].equals("x")) {
-					
-					temp[i][j+n] = arr[i][j+n]; //i,j에 x값을 넣음
-					arr[i][j+n] = arr[i][j];
-					arr[i][j] = temp[i][j+n];
+				if(i!=0 && arr[i-n][j].equals("x")) {
+					System.out.println("뭐 출력하게?"+(i-n)+","+j+","+arr[i-n][j]);
+					temp[i-n][j] = arr[i-n][j]; //i,j에 x값을 넣음
+					arr[i-n][j] = arr[i][j];
+					arr[i][j] = temp[i-n][j];	
+				}
+				else if(i>2 && arr[i][j].equals("x")) {
+					temp[i+n][j] = arr[i+n][j]; //i,j에 x값을 넣음
+					arr[i+n][j] = arr[i][j];
+					arr[i][j] = temp[i+n][j];
 				}	
 			}
 		}
 		return arr;
 	}
-	String[][] MoveL(String arr[][]) {          //뭐냐 이거!!
+	String[][] MoveR(String arr[][]) {
+		String[][] temp = new String[3][3];
+		int n;
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				n=1;
+				if(j==2 && arr[i][j].equals("x")) {
+					temp[i][j-n] = arr[i][j-n]; //i,j에 x값을 넣음
+					arr[i][j-n] = arr[i][j];
+					arr[i][j] = temp[i][j-n];
+					break;
+				}
+				else if(j<2 && arr[i][j+n].equals("x")) {     //내 앞이 비어 있으면
+					temp[i][j+n] = arr[i][j+n]; //i,j에 x값을 넣음
+					arr[i][j+n] = arr[i][j];
+					arr[i][j] = temp[i][j+n];
+					break;
+				}
+			}
+		}
+		return arr;
+	}
+	
+	String[][] MoveL(String arr[][]) {          
 		String[][] temp = new String[3][3];
 		int n=1;
 		for(int i=0;i<3;i++) {
 			for(int j=0;j<3;j++) {
-				if(j==0) n=0;
-				if(arr[i][j-n].equals("x")) {
-					temp[i][j-n] = arr[i][j-n];
-					arr[i][j-n] = arr[i][j];
-					arr[i][j] = temp[i][j-n];
+				if(j==0 && arr[i][j].equals("x")) {
+					temp[i][j+n] = arr[i][j+n];
+					arr[i][j+n] = arr[i][j];
+					arr[i][j] = temp[i][j+n];
+					break;
+				}else if(j>0 && arr[i][j-n].equals("x")) {
+					temp[i][j] = arr[i][j];
+					arr[i][j] = arr[i][j-n];
+					arr[i][j-n] = temp[i][j];
+					break;
 				}
 			}
-		}
-		for(int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
-				System.out.print(arr[i][j]+"\t");
-			}
-			System.out.println();
 		}
 		return arr;
 	}
@@ -95,7 +109,6 @@ class Check {
 		}
 		return count;
 	}
-	
 }
 
 public class by3_Game {
@@ -103,46 +116,49 @@ public class by3_Game {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String Check[][] = {{"1","2","3"},{"4","5","6"},{"7","8","x"}};
-		String Board[][] = {{"1","2","3"},{"4","5","6"},{"7","x","8"}};
+		String Board[][] = {{"x","2","3"},{"1","5","6"},{"7","8","4"}};
 		Move move = new Move();
 		Check check = new Check();
 		
 		check.Print(Board);
 		
-		System.out.println("[ Move ] a:Left  s:Right  w:Up  z:Down");
-		System.out.println("[ Exit ] k:Exit");
-		String user = s.next();
-		while(true) {
-			if(user.equals("k")) {
-				System.out.println("종료합니다.");
-				break;
-			}
-			if(check.Match(Board, Check)==9) {
-				System.out.println("정답입니다.");
-				break;
-			}
-			else if(user.equals("a")) {
-				move.MoveL(Board);
-				//check.Print(Board);
-				check.Match(Board, Check);
-				break;
-			}else if(user.equals("s")) {
-				move.MoveR(Board);
-				check.Print(Board);
-				check.Match(Board, Check);
-				break;
-			}else if(user.equals("w")) {
-				move.MoveU(Board);
-				//check.Print(Board);
-				check.Match(Board, Check);
-				break;
-			}else if(user.equals("z")) {
-				move.MoveD(Board);
-				//check.Print(Board);
-				check.Match(Board, Check);
-				break;
+		while(check.Match(Board, Check)!=9) {
+			System.out.println("[ Move ] a:Left  s:Right  w:Up  z:Down");
+			System.out.println("[ Exit ] k:Exit");
+			String user = s.next();
+			while(true) {
+				if(user.equals("k")) {
+					System.out.println("종료합니다.");
+					break;
+				}
+				if(check.Match(Board, Check)==9) {
+					System.out.println("정답입니다.");
+					break;
+				}
+				else if(user.equals("a")) {
+					move.MoveL(Board);
+					check.Print(Board);
+					check.Match(Board, Check);
+					break;
+				}else if(user.equals("s")) {
+					move.MoveR(Board);
+					check.Print(Board);
+					check.Match(Board, Check);
+					break;
+				}else if(user.equals("w")) {
+					move.MoveU(Board);
+					check.Print(Board);
+					check.Match(Board, Check);
+					break;
+				}else if(user.equals("z")) {
+					move.MoveD(Board);
+					check.Print(Board);
+					check.Match(Board, Check);
+					break;
+				}
 			}
 		}
+		
 		
 
 	}
