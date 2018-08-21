b09a625b5f5c16cbcc01498a983305eb3a3cdd18
YoonHee(S) <yh9189@gmail.com>
2018-08-21 18:32:32 +0900

    8월 21일 JSP

--- Java/Quiz/src/test.java
+++ Java/Quiz/src/test.java
@@ -1,22 +1,15 @@
-import java.util.Scanner;
-
 public class test {
-	
-	
 	public static void main(String[] args) {
-		String str = "/joinroom 123 111";
-		String temp = str.substring(str.indexOf(" "));
-		String pwd = temp.substring(temp.indexOf(" ")+1);
 		
-		String temp1 = str.substring(str.indexOf(" ")+1);
-		String friendName = temp1.substring(0,temp1.indexOf(" "));
-		String txt = temp1.substring(temp1.indexOf(" ")+1);
+		int i=0;
+		int j=0;
 		
-		System.out.println(temp);
-		System.out.println(pwd);
-		
-		System.out.println(temp1);
-		System.out.println(txt);
-		
+		// i=1
+		System.out.println(++i);
+		// j=0
+		System.out.println(j++);
+		// j=1
+		System.out.println(j);
+			
 	}
 }
